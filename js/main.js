d591a31f81632acf0564aa18e108c1ea8873e439
BwBest <raffine49@gmail.com>
2023-07-04 02:28:00 +0300

    Optimize javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 const generateButton = document.querySelector('#generate-joke');
 const textDisplay = document.querySelector('#modal h2');
 
-const req = new XMLHttpRequest();
-
-req.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    const data = JSON.parse(this.responseText);
-    displayJoke(data.value);
-    console.log(data);
-  }
-};
-
 function generateJoke() {
+  const req = new XMLHttpRequest();
+
   req.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+  req.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        const data = JSON.parse(this.responseText);
+        displayJoke(data.value);
+      } else {
+        displayJoke('An error occured (Not a joke)');
+      }
+    }
+  };
+
   req.send();
 }
 
@@ -20,5 +24,5 @@ function displayJoke(text) {
   textDisplay.textContent = text;
 }
 
-window.addEventListener('load', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
 generateButton.addEventListener('click', generateJoke);
